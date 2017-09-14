2ce982ac0dc931d4d4aa9a92cda3a7d7e65895c2
Ecohover Kuo <ecohover@gmail.com>
2017-09-14 14:40:17 +0800

    Add App.xaml.cs

---
+++ WPFDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string log4netPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config/log4net.config");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(log4netPath));
+#if DEBUG
+            WorkSpace fm = new WorkSpace();
+#else
+            StartPage fm = new StartPage();
+#endif
+            fm.Show();
+            base.OnStartup(e);
+        }
+    }
+}
