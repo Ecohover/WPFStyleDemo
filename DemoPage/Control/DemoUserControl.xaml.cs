38a762c9ea4f7afbe8492980916dc63a4ad30d6a
ecohover <ecohover@gmail.com>
2017-09-14 12:40:25 +0800

    Remove

--- DemoPage/Control/DemoUserControl.xaml.cs
+++ DemoPage/Control/DemoUserControl.xaml.cs
@@ -27,14 +27,8 @@ namespace WPFDemo.Control
         {
             UpdateStyle(style);
             InitializeComponent();
-            UpdateComdoBox();
-            UpdatDataGrid();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
 
         private void UpdateStyle(string style)
         {
@@ -54,61 +48,6 @@ namespace WPFDemo.Control
             {
 
             }
-        }
-        private void UpdatDataGrid()
-        {
-            ObservableCollection<DataGridData> list = new ObservableCollection<DataGridData>();
-
-            list.Add(new DataGridData("A", true));
-            list.Add(new DataGridData("B", true));
-            list.Add(new DataGridData("C", false));
-            list.Add(new DataGridData("D", true));
-            list.Add(new DataGridData("E", true));
-            list.Add(new DataGridData("F", true));
-            list.Add(new DataGridData("G", false));
-            list.Add(new DataGridData("H", false));
-            list.Add(new DataGridData("I", true));
-            list.Add(new DataGridData("J", true));
-            list.Add(new DataGridData("K", false));
-
-            gridtest.ItemsSource = list;
-        }
-
-        private void UpdateComdoBox()
-        {
-
-            ObservableCollection<TestList> cmbList = new ObservableCollection<TestList>();
-
-
-            cmbList.Add(new TestList("0", "C004-下拉式选单"));
-            cmbList.Add(new TestList("1", "1"));
-            cmbList.Add(new TestList("2", "2"));
-            cmbList.Add(new TestList("3", "3"));
-            cmbList.Add(new TestList("4", "4"));
-            cmbList.Add(new TestList("5", "5"));
-            cmbList.Add(new TestList("6", "6"));
-            cmbList.Add(new TestList("7", "7"));
-            cmbList.Add(new TestList("8", "8"));
-            cmbList.Add(new TestList("9", "9"));
-            cmbList.Add(new TestList("10", "10"));
-            cmbList.Add(new TestList("11", "1"));
-            cmbList.Add(new TestList("12", "2"));
-            cmbList.Add(new TestList("13", "3"));
-            cmbList.Add(new TestList("14", "4"));
-            cmbList.Add(new TestList("15", "5"));
-            cmbList.Add(new TestList("16", "6"));
-            cmbList.Add(new TestList("17", "7"));
-            cmbList.Add(new TestList("18", "8"));
-            cmbList.Add(new TestList("19", "9"));
-            cmbList.Add(new TestList("20", "10"));
-
-
-            cbStyleName.DisplayMemberPath = "Value";
-            cbStyleName.SelectedValuePath = "Key";
-            cbStyleName.ItemsSource = cmbList;
-            cbStyleName2.ItemsSource = cmbList;
-            cbStyleName3.ItemsSource = cmbList;
-            cbStyleName4.ItemsSource = cmbList;
         }
     }
 }
